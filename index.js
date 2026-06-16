30b30a92d10512912a75e32f8053280d78bc00cb
mishra-khushboo <khush27093@gmail.com>
2026-06-16 19:14:47 +0530

    Implemented PDF loading, chunking, Gemini embeddings setup, and Pinecone configuration

---
+++ index.js
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { Pinecone } from '@pinecone-database/pinecone';
+
+
+
+async function indexDocument() {
+	//load pdf  phase 1
+
+	const PDF_PATH = './dsa.pdf';
+	const pdfLoader = new PDFLoader(PDF_PATH);
+	const rawDocs = await pdfLoader.load();
+
+	//chunking
+	const textSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 1000,
+		chunkOverlap: 200,
+	});
+	const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+
+	//vector Embedding model
+	const embeddings = new GoogleGenerativeAIEmbeddings({
+		apiKey: process.env.GEMINI_API_KEY,
+		model: 'text-embedding-004',
+	});
+
+	//configure database
+	//Initialize Pinecone Client
+	const pinecone = new Pinecone();
+	const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+}
+
+
+
+indexDocument();
